786ba0f533258dd220e0f2d219af8e273f349de0
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-03-28 11:21:01 -0300

    test: ensure SignupController ruturns 400 if no name is provided

---
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from './SignUpController'
+
+describe('SignUpController', () => {
+  test('should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        // name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        password_confirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
