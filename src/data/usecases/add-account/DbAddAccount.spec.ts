3560426c668f6c56336b83bb15db9f0aa3d30221
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-04-10 19:59:45 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/DbAddAccount.spec.ts
+++ src/data/usecases/add-account/DbAddAccount.spec.ts
@@ -10,7 +10,7 @@ const makeAddAccountRepository = (): AddAccountRepository => {
   class AddAccountRepositoryStub implements AddAccountRepository {
     async add(accountData: AddAccountModel): Promise<AccountModel> {
       const fakeAccount = {
-        id: 'valid_ud',
+        id: 'valid_id',
         name: 'valid_name',
         email: 'valid_email',
         password: 'hashed_password'
@@ -119,5 +119,24 @@ describe('DbAddAccount Usecase', () => {
     const promise = sut.add(accountData)
 
     await expect(promise).rejects.toThrow()
+  })
+
+  test('should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
   })
 })
