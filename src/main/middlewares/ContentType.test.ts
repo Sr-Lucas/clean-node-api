4d30e28ded0d1717ee801328f2dd1064bb50de6d
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-07-04 15:00:23 -0300

    test: ensure requests return xml when forced

--- src/main/middlewares/ContentType.test.ts
+++ src/main/middlewares/ContentType.test.ts
@@ -10,4 +10,13 @@ describe('Content Type Middleware', () => {
 
     await request(app).get('/testContentType').expect('content-type', /json/)
   })
+
+  test('should return xml content type when forced', async () => {
+    app.get('/testContentTypeXml', (request, response) => {
+      response.type('xml')
+      response.send('')
+    })
+
+    await request(app).get('/testContentTypeXml').expect('content-type', /xml/)
+  })
 })
