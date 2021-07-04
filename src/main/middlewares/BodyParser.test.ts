105e6ff41b49fd28fb4f1a30b8666c404f31035b
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-07-04 14:35:50 -0300

    test: ensure requests parse body as json

---
+++ src/main/middlewares/BodyParser.test.ts
@@ -0,0 +1,16 @@
+import { response } from 'express'
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body Parser Middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/testBodyParser', (request, response) => {
+      response.send(request.body)
+    })
+
+    await request(app)
+      .post('/testBodyParser')
+      .send({ name: 'Lucas' })
+      .expect({ name: 'Lucas' })
+  })
+})
