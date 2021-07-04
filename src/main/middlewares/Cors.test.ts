319cc07997fbd5f2f268bd22aef434256e90e415
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-07-04 14:57:43 -0300

    refactor: changes http method to get in cors middleware

--- src/main/middlewares/Cors.test.ts
+++ src/main/middlewares/Cors.test.ts
@@ -4,12 +4,12 @@ import app from '../config/app'
 
 describe('CORS Middleware', () => {
   test('should enable cors', async () => {
-    app.post('/testCors', (request, response) => {
+    app.get('/testCors', (request, response) => {
       response.send()
     })
 
     await request(app)
-      .post('/testCors')
+      .get('/testCors')
       .expect('access-control-allow-origin', '*')
       .expect('access-control-allow-methods', '*')
       .expect('access-control-allow-headers', '*')
