37828ce4553c07dcd7155ec9436867136ea566ad
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-07-11 13:03:55 -0300

    refactor: returns error message on response

--- src/main/adapters/ExpressRouteAdapter.ts
+++ src/main/adapters/ExpressRouteAdapter.ts
@@ -9,6 +9,12 @@ export const adaptRoute = (controller: Controller) => {
 
     const httpResponse = await controller.handle(httpRequest)
 
+    if (httpResponse.statusCode !== 200) {
+      response
+        .status(httpResponse.statusCode)
+        .json({ error: httpResponse.body.message })
+    }
+
     return response.status(httpResponse.statusCode).json(httpResponse.body)
   }
 }
